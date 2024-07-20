93a7f17d2c9190568dbfbffa4b1c825f40b8f723
Divyam19 <divyamgupta19@gmail.com>
2024-07-20 15:52:14 +0530

    implemented cards in products

--- src/app/(app)/addproducts/Products.tsx
+++ src/app/(app)/addproducts/Products.tsx
@@ -2,6 +2,14 @@
 import React, { useEffect, useState } from 'react'
 import { findProducts } from '@/actions/action';
 import { useSession } from 'next-auth/react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 interface Product {
   id: string;
@@ -45,11 +53,22 @@ const Products = () => {
       {isLoading && <p>Loading products...</p>}
       {error && <p>Error: {error.message}</p>}
       {products.length > 0 && (
-        <ul>
+        <ul className='flex flex-col gap-4'>
           {products.map((product: Product) => (
-            <li key={product.id}>
-              <h1>{product.name}</h1>
-              {/* You can add other product details here */}
+            <li key={product.id} className=''>
+              <Card>
+                <CardHeader>
+                  <CardTitle>{product.name}</CardTitle> 
+                  <CardDescription>{product.description}</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p>{product.type}</p>
+                  <p>Rs. {product.price}./</p>
+                </CardContent>
+                <CardFooter>
+                  <p>Card Footer</p>
+                </CardFooter>
+              </Card>
             </li>
           ))}
         </ul>
