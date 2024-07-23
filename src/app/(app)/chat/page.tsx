12dda6f0b59b02ffbd3494aeedbd3bb744bf46da
Divyam19 <divyamgupta19@gmail.com>
2024-07-23 16:00:18 +0530

    product push to model done

--- src/app/(app)/chat/page.tsx
+++ src/app/(app)/chat/page.tsx
@@ -1,16 +1,38 @@
 'use client'
+
 import React, { useState, useEffect } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import ChatHistory from "./ChatHistory";
 import Loading from "@/app/loading";
+import { getproducts } from "@/lib/api/queries";
 
 const Chat = () => {
+  const [products, setProducts] = useState([]);
   const [userInput, setUserInput] = useState("");
   const [chatHistory, setChatHistory] = useState<any[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [defaultMessage, setDefaultMessage] = useState("Hi, I'm Sol Mate. I'm here to solve all your energy needs. How can I help you? \n Ask me anything.");
+  const [isProductsLoading, setIsProductsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsProductsLoading(true);
+        const response = await getproducts();
+        const data = JSON.parse(response);
+        setProducts(data);
+        
+        setChatHistory([{ type: "bot", message: "Hi, I'm Sol Mate. I'm here to solve all your energy needs. How can I help you?" }]);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+        setChatHistory([{ type: "bot", message: "Hi, I'm Sol Mate. I'm here to solve all your energy needs. How can I help you?" }]);
+      } finally {
+        setIsProductsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
 
   const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY as string);
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro-latest" });
@@ -25,20 +47,21 @@ const Chat = () => {
     }
     setIsLoading(true);
     try {
-      // Prepare the conversation history
-      const conversationHistory = chatHistory.map(entry => 
+      const conversationHistory = chatHistory.map(entry =>
         `${entry.type === 'user' ? 'Human' : 'AI'}: ${entry.message}`
       ).join('\n');
 
-      // Combine conversation history with the new user input
-      const prompt = `${conversationHistory}\nHuman: ${userInput}\nAI:`;
+      // Append product information to the user's input
+      const enhancedUserInput = `${userInput}\n\nAvailable products: ${JSON.stringify(products)}`;
+
+      const prompt = `${conversationHistory}\nHuman: ${enhancedUserInput}\nAI:`;
 
       const result = await model.generateContent(prompt);
       const response = await result.response;
-      
+
       setChatHistory((prevHistory) => [
         ...prevHistory,
-        { type: "user", message: userInput },
+        { type: "user", message: userInput }, // Only show original user input in chat history
         { type: "bot", message: response.text() },
       ]);
     } catch (error) {
@@ -54,17 +77,14 @@ const Chat = () => {
     setUserInput("");
   };
 
-  useEffect(() => {
-    setChatHistory([
-      { type: "bot", message: defaultMessage },
-    ]);
-  }, []);
-
   return (
     <div className="container m-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-4">Sol Mate</h1>
       <div className="chat-container rounded-lg shadow-md p-4">
-        <ChatHistory chatHistory={chatHistory} />
+        {isProductsLoading ? (
+          <Loading />
+        ) : (
+          <ChatHistory chatHistory={chatHistory} />
+        )}
         {isLoading && <Loading />}
       </div>
       <div className="flex mt-4">
@@ -78,11 +98,14 @@ const Chat = () => {
         <Button
           className="px-4 py-2 ml-2 rounded-lg bg-blue-500 text-black hover:bg-blue-600 focus:outline-none"
           onClick={sendMessage}
-          disabled={isLoading}
+          disabled={isLoading || isProductsLoading}
         >
           Send
         </Button>
-        <Button className="px-4 py-2 ml-2 rounded-lg bg-red-400 hover:bg-red-600 rounded-xl" onClick={clearChat}>
+        <Button 
+          className="px-4 py-2 ml-2 rounded-lg bg-red-400 hover:bg-red-600 rounded-xl" 
+          onClick={clearChat}
+        >
           Clear Chat
         </Button>
       </div>
@@ -91,3 +114,231 @@ const Chat = () => {
 };
 
 export default Chat;
+
+
+
+
+// 'use client'
+
+// import React, { useState, useEffect } from "react";
+// import { Input } from "@/components/ui/input";
+// import { Button } from "@/components/ui/button";
+// import { GoogleGenerativeAI } from "@google/generative-ai";
+// import ChatHistory from "./ChatHistory";
+// import Loading from "@/app/loading";
+// import { getproducts } from "@/lib/api/queries";
+// import { get } from "http";
+
+// const Chat = () => {
+//   const [products, setProducts] = useState([]);
+//   const productss = JSON.stringify(products);
+//   const [userInput, setUserInput] = useState("");
+//   const [chatHistory, setChatHistory] = useState<any[]>([]);
+//   const [isLoading, setIsLoading] = useState<boolean>(false);
+//   const [defaultMessage, setDefaultMessage] = useState(`Hi, I'm Sol Mate. I'm here to solve all your energy needs. How can I help you? \n Ask me anything. from the list of products below.${JSON.stringify(productss)}`);
+//   console.log(JSON.stringify(productss));
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const response = await getproducts();
+//       const data = JSON.parse(response);
+//       setProducts(data);
+//     };
+//     fetchData();
+//     setChatHistory([
+//       { type: "bot", message: `Hi, I'm Sol Mate. I'm here to solve all your energy needs. How can I help you? \n Ask me anything. I know about ${JSON.stringify(products)} products.` },
+//     ]);
+//   }, []);
+
+//   // console.log(products);
+//   const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY as string);
+//   const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro-latest" });
+
+//   const handleUserInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     setUserInput(e.target.value);
+//   };
+
+//   const sendMessage = async () => {
+//     if (userInput.trim() === "") {
+//       return;
+//     }
+//     setIsLoading(true);
+//     try {
+//       const conversationHistory = chatHistory.map(entry =>
+//         `${entry.type === 'user' ? 'Human' : 'AI'}: ${entry.message}`
+//       ).join('\n');
+
+//       const prompt = `${conversationHistory}\nHuman: ${userInput}\nAI:`;
+
+//       const result = await model.generateContent(prompt);
+//       const response = await result.response;
+
+//       setChatHistory((prevHistory) => [
+//         ...prevHistory,
+//         { type: "user", message: userInput },
+//         { type: "bot", message: response.text() },
+//       ]);
+//     } catch (error) {
+//       console.error("Error sending content:", error);
+//     } finally {
+//       setUserInput("");
+//       setIsLoading(false);
+//     }
+//   };
+
+//   const clearChat = () => {
+//     setChatHistory([]);
+//     setUserInput("");
+//   };
+
+//   useEffect(() => {
+//     setChatHistory([
+//       { type: "bot", message: defaultMessage },
+//     ]);    
+//   }, []);
+
+//   return (
+//     <div className="container m-auto px-4 py-8">
+//       <div className="chat-container rounded-lg shadow-md p-4">
+//         <ChatHistory chatHistory={chatHistory} />
+//         {isLoading && <Loading />}
+//       </div>
+//       <div className="flex mt-4">
+//         <Input
+//           type="text"
+//           className="flex-grow px-4 py-2 rounded-lg border border-red-500 focus:outline-none focus:ring-2 focus:ring-red-600"
+//           placeholder="Type your message"
+//           value={userInput}
+//           onChange={handleUserInput}
+//         />
+//         <Button
+//           className="px-4 py-2 ml-2 rounded-lg bg-blue-500 text-black hover:bg-blue-600 focus:outline-none"
+//           onClick={sendMessage}
+//           disabled={isLoading}
+//         >
+//           Send
+//         </Button>
+//         <Button 
+//           className="px-4 py-2 ml-2 rounded-lg bg-red-400 hover:bg-red-600 rounded-xl" 
+//           onClick={clearChat}
+//         >
+//           Clear Chat
+//         </Button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Chat;
+
+
+// 'use client'
+
+// import React, { useState, useEffect } from "react";
+// import { Input } from "@/components/ui/input";
+// import { Button } from "@/components/ui/button";
+// import { GoogleGenerativeAI } from "@google/generative-ai";
+// import ChatHistory from "./ChatHistory";
+// import Loading from "@/app/loading";
+// import { getproducts } from "@/lib/api/queries";
+// import { get } from "http";
+
+// const Chat = () => {
+//   const [products, setProducts] = useState([]);
+
+//   const [userInput, setUserInput] = useState("");
+//   const [chatHistory, setChatHistory] = useState<any[]>([{ type: "bot", message: `Hi, I'm Sol Mate. I'm here to solve all your energy needs. How can I help you? \n Ask me anything. I know about ${products} products.` }]);
+//   const [isLoading, setIsLoading] = useState<boolean>(false);
+//   const [defaultMessage, setDefaultMessage] = useState("Hi, I'm Sol Mate. I'm here to solve all your energy needs. How can I help you? \n Ask me anything.");
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const response = await getproducts();
+//       const data = JSON.parse(response);
+//       setProducts(data);
+//     };
+//     fetchData();
+//     setChatHistory([
+//       { type: "bot", message: `Hi, I'm Sol Mate. I'm here to solve all your energy needs. How can I help you? \n Ask me anything. I know about ${JSON.stringify(products)} products.` },
+//     ]);
+//   }, []);
+
+//   // console.log(products);
+//   const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY as string);
+//   const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro-latest" });
+
+//   const handleUserInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     setUserInput(e.target.value);
+//   };
+
+//   const sendMessage = async () => {
+//     if (userInput.trim() === "") {
+//       return;
+//     }
+//     setIsLoading(true);
+//     try {
+//       const conversationHistory = chatHistory.map(entry =>
+//         `${entry.type === 'user' ? 'Human' : 'AI'}: ${entry.message}`
+//       ).join('\n');
+
+//       const prompt = `${conversationHistory}\nHuman: ${userInput}\nAI:`;
+
+//       const result = await model.generateContent(prompt);
+//       const response = await result.response;
+
+//       setChatHistory((prevHistory) => [
+//         ...prevHistory,
+//         { type: "user", message: userInput },
+//         { type: "bot", message: response.text() },
+//       ]);
+//     } catch (error) {
+//       console.error("Error sending content:", error);
+//     } finally {
+//       setUserInput("");
+//       setIsLoading(false);
+//     }
+//   };
+
+//   const clearChat = () => {
+//     setChatHistory([]);
+//     setUserInput("");
+//   };
+
+//   useEffect(() => {
+//     setChatHistory([
+//       { type: "bot", message: defaultMessage },
+//     ]);    
+//   }, []);
+
+//   return (
+//     <div className="container m-auto px-4 py-8">
+//       <div className="chat-container rounded-lg shadow-md p-4">
+//         <ChatHistory chatHistory={chatHistory} />
+//         {isLoading && <Loading />}
+//       </div>
+//       <div className="flex mt-4">
+//         <Input
+//           type="text"
+//           className="flex-grow px-4 py-2 rounded-lg border border-red-500 focus:outline-none focus:ring-2 focus:ring-red-600"
+//           placeholder="Type your message"
+//           value={userInput}
+//           onChange={handleUserInput}
+//         />
+//         <Button
+//           className="px-4 py-2 ml-2 rounded-lg bg-blue-500 text-black hover:bg-blue-600 focus:outline-none"
+//           onClick={sendMessage}
+//           disabled={isLoading}
+//         >
+//           Send
+//         </Button>
+//         <Button 
+//           className="px-4 py-2 ml-2 rounded-lg bg-red-400 hover:bg-red-600 rounded-xl" 
+//           onClick={clearChat}
+//         >
+//           Clear Chat
+//         </Button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Chat;
--- src/lib/api/queries.ts
+++ src/lib/api/queries.ts
@@ -1,12 +1,19 @@
+'use server'
 import { getUserAuth } from "../auth/utils";
+import { db } from "../db";
 
-export async function getsellerdata(){
-    const {session}=await getUserAuth();
-    const sellerid=session?.user.id;
-    const email=session?.user.email;
-    const mobno=db?.seller.findUnique({where:{id:sellerid}}).mobno
-    const city=db?.seller.findUnique({where:{id:sellerid}}).city
-    const country=db?.seller.findUnique({where:{id:sellerid}}).country
-    const state=db?.seller.findUnique({where:{id:sellerid}}).state
-    return {sellerdata:{sellerid,email,mobno,city,country,state}}
-}
+// export async function getsellerdata(){
+//     const {session}=await getUserAuth();
+//     const sellerid=session?.user.id;
+//     const email=session?.user.email;
+//     const mobno=db?.seller.findUnique({where:{id:sellerid}}).mobno
+//     const city=db?.seller.findUnique({where:{id:sellerid}}).city
+//     const country=db?.seller.findUnique({where:{id:sellerid}}).country
+//     const state=db?.seller.findUnique({where:{id:sellerid}}).state
+//     return {sellerdata:{sellerid,email,mobno,city,country,state}}
+// }
+
+export async function getproducts() {
+    const products = await db!.product.findMany();
+    return JSON.stringify(products);
+  }
